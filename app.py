4bf68c6d35e0a82b7d987ba327e0e2949f35b317
bgioggia <bgioggia@gmail.com>
2021-07-27 17:37:58 -0400

    Basic flask/sql app set up. Can continue with adding features and functionality

---
+++ app.py
@@ -0,0 +1,31 @@
+import sqlite3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route('/')
+def render_index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/render_beers')
+def render_beers():
+    return render_template('beers.html')
+
+@app.route('/render_about')
+def render_about():
+    return render_template('beers.html')
+
+
+if __name__ == '__main__':
+    app.run()
